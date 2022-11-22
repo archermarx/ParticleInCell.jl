61aceda5e85a35cd53457d0d75ac9846d3507c4f
Thomas Marks <marksta@umich.edu>
2022-11-22 12:02:02 -0500

    beam plasma done

--- worksheets/worksheet_7.jl
+++ worksheets/worksheet_7.jl
@@ -19,7 +19,7 @@ const PLOT_SCALING_OPTIONS = (;
 begin
     # Problem 1: Two-stream instability
 
-    function two_stream_instability(N = 512, N_ppc = 256)
+    function two_stream_instability(N = 64, N_ppc = 16)
         N_p = N * N_ppc
         xmin = 0.0
         xmax = 8π*√(3)
@@ -49,9 +49,52 @@ begin
         return ParticleInCell.simulate(particles, fields, grid; Δt, tmax)
     end
 
-    t, x, xs, vxs, vys, ns, Es = two_stream_instability()
+    N = 128
+    N_ppc = 16 
+    t, x, xs, vxs, vys, ns, Es = two_stream_instability(N, N_ppc)
     vlims = (-8, 8)
-    ParticleInCell.animate_vdf(xs, vxs; ts = t, dir = WS7_RESULTS_DIR, type = "1D", suffix = "twostream", vlims)
+    #ParticleInCell.animate_vdf(xs, vxs; ts = t, dir = WS7_RESULTS_DIR, type = "1D", suffix = "twostream_N=$(N)_Nppc=$(N_ppc)", vlims)
     ParticleInCell.plot_vdf(xs[:, 1], vxs[:, 1]; type = "1D", vlims) |> display
     ParticleInCell.plot_vdf(xs[:, end], vxs[:, end]; type = "1D", vlims) |> display
+end
+
+begin
+    # Problem 2: beam plasma (bump on tail) instability
+    function bump_on_tail(N, N_ppc)
+        N_p = N * N_ppc
+        v_th = 0.05
+        v_d = 3.0
+        n_b_ratio = 10
+        xmin = 0.0
+        xmax = π
+        tmin = 0.0
+        tmax = 30π
+        Δt = 0.2
+
+        particles, fields, grid = ParticleInCell.initialize(
+            N_p, N_p, N, xmin, xmax; charge_per_particle = 1
+        )
+
+        ParticleInCell.maxwellian_vdf!(particles, v_th)
+
+        # Assign one in n_b_ratio particles to the beam
+        for i in 1:n_b_ratio:N_p
+            particles.vx[i] = v_d
+            particles.vy[i] = 0.0
+        end
+
+        return ParticleInCell.simulate(particles, fields, grid; Δt, tmax)
+    end
+
+    N = 128
+    N_ppc = 128
+    t, x, xs, vxs, vys, ns, Es = bump_on_tail(N, N_ppc)
+    vlims = (-0.5, 3.5)
+    ParticleInCell.animate_vdf(xs, vxs; ts = t, dir = WS7_RESULTS_DIR, frameskip = 1, type = "1D", suffix = "beamplasma_N=$(N)_Nppc=$(N_ppc)", vlims)
+    ParticleInCell.plot_vdf(xs[:, 1], vxs[:, 1]; type = "1D", vlims) |> display
+    ParticleInCell.plot_vdf(xs[:, end], vxs[:, end]; type = "1D", vlims) |> display
+end
+
+begin
+    # Problem 3: Landau damping
 end
